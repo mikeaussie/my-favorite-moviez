585c03a5e1197478dbe282940b1aa802fc621731
mikeaussie <michael.pyatin@me.com>
2016-04-09 20:36:52 +1000

    added back buttons in all VCs

--- my-favorite-moviez/DescVC.swift
+++ my-favorite-moviez/DescVC.swift
@@ -7,29 +7,27 @@
 //
 
 import UIKit
+import CoreData
 
 class DescVC: UIViewController {
 
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var movieImg: UIImageView!
+    @IBOutlet weak var descLbl: UILabel!
+    @IBOutlet weak var storyLbl: UILabel!
+    @IBOutlet weak var webLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func backBtn(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
